a6bba4d2a339f844d5bd9ccd82d66a9c89bfd9b7
qingfengxia <qingfeng.xia@ukaea.uk>
2020-06-30 22:44:19 +0100

    remove EERA model hdf5 IO demo

--- EERA_pipeline_demo/data_client_example.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "Restful.h"
-
-int main()
-{
-    std::string models_url = "http://data.scrc.uk/api/model/";
-    auto j = Restful::get_json(models_url);
-    std::cout << j.dump(4) << std::endl;
-}
